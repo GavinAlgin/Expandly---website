ea39ecf5ce3ce8f022a57356c465fb935d867237
Gavin Algin <gavinkidart65@gmail.com>
2025-04-21 21:32:24 +0200

    testimonals

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,11 @@ function App() {
     <Services />
     <Projects />
     <Testinomals />
+    <div className="bg-black h-1/2 flex text-center">
+      <p className="text-sm uppercase font-bold">Now available for hire</p>
+      <h2 className="text-2xl font-semibold">Get in touch</h2>
+      <form action=""></form>
+    </div>
     </>
   )
 }
